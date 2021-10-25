548c20e492426a700570a9a32833e5f010e9bda9
ArmineGyulazyan <86404976+ArmineGyulazyan@users.noreply.github.com>
2021-10-25 13:25:37 +0300

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -1,41 +1,37 @@
-import java.util.Arrays;
 import java.util.Scanner;
-public class Sorting {
 
+public class Sorting1 {
 
-    public void sort(int[] array) {
-        var temp = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 1; j < array.length - 1; j += 2) {
-
-                if((array[j] * array[j++]) !=0 ) {
-                        temp = array[j];
-                        array[j] = array[j -= 1];
-                        array[j -= 1] = temp;
-                    }
-
-                if (array[j] == 1 && array[j++] == 0 || array.length < 4) {
-                    System.out.println("No sorting:");
-                }
-            }
-        }
+    public static void sort(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+         array[j] = temp;
     }
-        public static void main (String[] args){
-            int n;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the number of elements: ");
-            n = sc.nextInt();
+
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        n = sc.nextInt();
+        if(n<4)
+        {System.out.println("Cannot Sort!");}
+
+        else {
+            int[] arr = new int[n];
             System.out.println("Enter the elements: ");
-            int[] array = new int[n];
             int i;
             for (i = 0; i < n; i++) {
-                array[i] = sc.nextInt();
+                arr[i] = sc.nextInt();
             }
+            int j = arr.length / 2;
+            for (i = 0; i < arr.length / 2; i++) {
+                sort(arr, i, j);
+                j++;
+            }
+            for (i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
 
-            var sorter = new Sorting();
-            sorter.sort(array);
-
-            System.out.println(Arrays.toString(array));
+            }
         }
     }
-
+}
