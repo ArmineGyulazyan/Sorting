8dc75488f84a30e23ed969246005b6424c155063
ArmineGyulazyan <86404976+ArmineGyulazyan@users.noreply.github.com>
2021-11-09 23:52:38 +0300

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -1,10 +1,16 @@
 import java.util.Arrays;
 import java.util.Scanner;
-//j= arr.length-1;
+
 
     public class Sorting {
 
-                    public static void sort(char[] array, int i, int j) {
+        private char[] array;
+
+        public void setArray(char[] array) {
+            this.array = array;
+        }
+
+        public static void sort(char[] array, int i, int j) {
                         char temp1 = array[i];
                         char temp2 = array[i++];
                         array[i] = array[j--];
@@ -14,29 +20,37 @@ import java.util.Scanner;
                     }
 
                     public static void main (String[] args) {
-                int n;
-                Scanner sc = new Scanner(System.in);
-                System.out.println("Enter the number of elements: ");
-                n = sc.nextInt();
+                        int n;
+                        Scanner sc = new Scanner(System.in);
+                        System.out.println("Enter the number of elements: ");
+                        n = sc.nextInt();
 
-                char[] arr = new char[n];
-                System.out.println("Enter the elements: ");
+                        char[] arr = new char[n];
+                        System.out.println("Enter the elements: ");
 
-                int i;
-                for (i = 0; i < n; i++) {
+                        int i;
+                        for (i = 0; i < n; i++) {
 
-                    arr[i] = sc.next().charAt(0);
-                }
-                int j = arr.length;
-                for (i = 0; i < arr.length; i++) {
-                    sort(arr, i, j);
-                    j--;
-                }
-                for (i = 0; i < n; i++) {
-                    System.out.print(arr[i] + " ");
+                            arr[i] = sc.next().charAt(0);
+                        }
+                        int j = arr.length - 1;
+                        for (i = 0; i < arr.length; i++) {
+                            if (arr[i] == '0' && arr[i++] == '1') {
+                                 if (arr[j -= 2] == 1) {
+                                    sort(arr, i++, j);
+                                     System.out.println("omg");
+                                }
+                                else
+                                    System.out.println("hey");
+                            }
 
-                }
-            }
+                            //j--;
+                        }
+                        System.out.println(Arrays.toString(arr));
+                        /** for (i = 0; i < n; i++) {
+                         System.out.print(arr[i] + " ");
+
+                         }**/
+                    }
     }
-
 
