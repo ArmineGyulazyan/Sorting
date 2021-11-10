56c201f42da9b4185b882822813f6a97d880abe3
ArmineGyulazyan <86404976+ArmineGyulazyan@users.noreply.github.com>
2021-11-11 01:10:07 +0300

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -7,19 +7,29 @@ import java.util.Scanner;
         private char[] array;
 
         public void setArray(char[] array) {
+
             this.array = array;
         }
 
         public static void sort(char[] array, int i, int j) {
                         char temp1 = array[i];
-                        char temp2 = array[i++];
-                        array[i] = array[j--];
-                        array[i++] = array[j];
-                        array[j--]=temp1;
+                        char temp2 = array[i+1];
+                        array[i] = array[j-1];
+                        array[i+1] = array[j];
+                        array[j-1]=temp1;
                         array[j]=temp2;
                     }
 
+      /** public boolean isSorted(char[] array,int i, int j) {
+            for ( i = 0; i < array.length - 1; i++, j--) {
+                if (array[i] < array[j])
+                    return false;
+            }
+            return true;
+        }**/
+
                     public static void main (String[] args) {
+
                         int n;
                         Scanner sc = new Scanner(System.in);
                         System.out.println("Enter the number of elements: ");
@@ -34,14 +44,12 @@ import java.util.Scanner;
                             arr[i] = sc.next().charAt(0);
                         }
                         int j = arr.length - 1;
-                        for (i = 0; i < arr.length; i++) {
-                            if (arr[i] == '0' && arr[i++] == '1') {
-                                 if (arr[j -= 2] == 1) {
-                                    sort(arr, i++, j);
-                                     System.out.println("omg");
-                                }
-                                else
-                                    System.out.println("hey");
+                        for (i = 0; i < j; i++) {
+                            if (arr[i] == '0' && arr[i+1] == '1') {
+                                 if (arr[j - 2] == '1') {
+                                    sort(arr, i+1, j);
+                                 }
+                                  //if(arr[i]==)
                             }
 
                             //j--;
