255e34c14de4db7c361c4aa74f7667a4d7576e5a
ArmineGyulazyan <86404976+ArmineGyulazyan@users.noreply.github.com>
2021-11-19 01:51:22 +0300

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
-public class Sort1  {
+public class Sorting  {
 
     public static void sort(char[] array, int firstp, int secondp) {
         char temp1 = array[firstp];
@@ -38,88 +38,93 @@ public class Sort1  {
 
             array[i] = sc.next().charAt(0);
         }
-        //int j = array.length ;
 
-
-        // int j = array.length ;
-        /** boolean t = true;
-         while(t==true){
-         t=false;
-         }**/
         for (i = 0; i <=array.length-1; i++) {
 
-
+            System.out.println( Arrays.toString(array));
             if( isSorted(array, i)){
 
                 System.out.println("Sorted! "+ Arrays.toString(array));
                 System.exit(0);
             }
+            //System.out.println( Arrays.toString(array));
 
-            System.out.println( Arrays.toString(array));
-            // else {
             if(array.length == 5) {
 
+            if(i==array.length-1){
+
+               if(array[i-1]=='0' && array[i]=='1' ){
+                      sort(array, i - 1, i - 4);
+                   i=0;
+                  }
+              }
                 if(i == array.length-2){
-                    if(array[i]=='0') {
+
+                    if(array[i]=='0' && array[i+1]=='1' && array[i-1]=='1' ) {
+
+                        sort(array, i, i - 3);
+                        i=0;
+                    }
+                    else if(array[i]=='0' && array[i+1]=='1'){
+                        sort(array, i - 1, i - 3);
+                    }
+                    else if(array[i]=='0' && array[i+1]=='0'){
+                        sort(array, i, i - 3);
+                    }
+                    else if(array[i]=='1' && array[i+1]=='1'){
+                        sort(array, i-1, i - 3);
+                    }
+                    else{
+                       // sort(array, i - 2, i);
                         sort(array, i + 1, i - 3);
                     }
-                        else{
-                            sort(array, i-1, i - 3);
-                        }
-                    }
-                  //  if()
-                
+                }
                 else{
-                if (array[i] == '0' && array[i + 1] == '1') {
+                    if (array[i] == '0' && array[i + 1] == '1') {
 
-                    if(i+1 == array.length-1) {
-                        if(array[i+1] == '1') {
-                            sort(array, i - 1, i - 3);
-                        }
-                        else if(array[i+1] == '0'){
-                            sort(array, i , i - 3);
-                        }
-
-                    }
-
-                    else{
-
-                        if(array[i+2]=='0'){
-                            if(i+1 == array.length-3){
-                                sort(array, i, i+2);
+                        if(i+1 == array.length-1) {
+                            if(array[i+1] == '1') {
+                                sort(array, i - 1, i - 3);
                             }
-                            else if(i+1 == array.length-4){
-                                sort(array,i, array.length-2);
+                            else if(array[i+1] == '0'){
+                                sort(array, i , i - 3);
                             }
-                            else{
-                                sort(array, i, i-2);
-                            }
-                        }
-                        if ( i+2==array.length){
-                            if (array[i-1] == '0' ){
-                                sort(array, i-1, i-3);
-                            }
-                        }
-                        if (array[i + 2] == '1') {         //i+2y 6a vercnum, vorn el chka
-                            if(i+2==array.length-1){
-                                sort(array, i+1, i-2);
-                            }
-                            else if(i+2== array.length-3 && array[i+3] == '1'){
-                                sort(array, i+1, array.length-2);
-                            }
-
-                            else {
-                                sort(array, i + 1,  array.length-2); }
-
 
                         }
 
+                        else{
+
+                            if(array[i+2]=='0'){
+                                if(i+1 == array.length-3){
+                                    sort(array, i, i+2);
+                                }
+                                else if(i+1 == array.length-4){
+                                    sort(array,i, array.length-2);
+                                }
+                                else{   //firsr change correct
+                                    sort(array, i, i-2);
+                                }
+                            }
+                            if ( i+2==array.length){
+                                if (array[i-1] == '0' ){
+                                    sort(array, i-1, i-3);
+                                }
+                            }
+                            if (array[i + 2] == '1') {         //i+2y 6a vercnum, vorn el chka
+                                if(i+2==array.length-1){
+                                    sort(array, i+1, i-2);
+                                }
+                                else if(i+2== array.length-3 && array[i+3] == '1'){
+                                    sort(array, i+1, array.length-2);
+                                }
+
+                                else {
+                                    sort(array, i + 1,  array.length-2); }
+                            }
+                        }
                     }
                 }
-                }
-
             }
         }
-
     }
 }
