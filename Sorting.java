2205bae06207d27276e1f55b9c31ad6516816321
ArmineGyulazyan <86404976+ArmineGyulazyan@users.noreply.github.com>
2021-11-21 01:19:26 +0300

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -1,33 +1,37 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Sort1  {
+public class Sorting {
 
     public static void sort(char[] array, int firstp, int secondp) {
         char temp1 = array[firstp];
-        char temp2= array[firstp+1];
+        char temp2 = array[firstp + 1];
         array[firstp] = array[secondp];
-        array[firstp+1] = array[secondp+1];
-        array[secondp]=temp1;
-        array[secondp+1]=temp2;
+        array[firstp + 1] = array[secondp + 1];
+        array[secondp] = temp1;
+        array[secondp + 1] = temp2;
     }
-    public static void cut(char[] array, int i){
-        for (i = 0; i < array.length-1; i++) {
-            if(array[i] > array[i + 1]){
 
-            }
-        }
-    }
+    /**
+     * public static void cut(char[] array, int i){
+     * for (i = 0; i < array.length-1; i++) {
+     * if(array[i] > array[i + 1]){
+     * <p>
+     * }
+     * }
+     * }
+     **/
 
     public static boolean isSorted(char[] array, int i) {
-        for (i = 0; i < array.length-1; i++) {
+        for (i = 0; i < array.length - 1; i++) {
             if (array[i] > array[i + 1]) {
                 return false;
             }
         }
         return true;
     }
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
 
         int n;
         Scanner sc = new Scanner(System.in);
@@ -42,87 +46,131 @@ public class Sort1  {
 
             array[i] = sc.next().charAt(0);
         }
-        for (i = 0; i <=array.length-1; i++) {
+
+        for (i = 0; i <= array.length - 1; i++) {
 
             System.out.println( Arrays.toString(array));
-            if( isSorted(array, i)){
+            /** if(array.length>5){
 
-                System.out.println("Sorted! "+ Arrays.toString(array));
-                System.exit(0);
-            }
-            //System.out.println( Arrays.toString(array));
-            if(array.length>5){
-
-            }
-            if(array.length == 5) {
-                if(i==array.length-1){
-
-                    if(array[i-1]=='0' && array[i]=='1' ){
-                        sort(array, i - 1, i - 4);
-                        i=0;
-                    }
-                }
-                if(i == array.length-2){
-                    if(array[i]=='0' && array[i+1]=='1' && array[i-1]=='1' ) {
+             }**/
+            if (array.length == 5) {
+                if (i == array.length - 2) {
+                    if (array[i] == '0' && array[i + 1] == '1' && array[i - 1] == '1') {
                         sort(array, i, i - 3);
-                        i=0;
+                        i = 0;
                     }
-                    else if(array[i]=='0' && array[i+1]=='1'){
-                        sort(array, i - 1, i - 3);
-                    }
-                    else if(array[i]=='0' && array[i+1]=='0'){
+                    /**
+                     * 01 i-1,i-3
+                     * 00 i,i-3
+                     * 11 i-1,i-3**/
+                   else if(array[i]=='1' && array[i+1]=='0'){
+                     sort(array, i - 1, i - 3);
+                     }
+                     else if(array[i]=='0' && array[i+1]=='1'){
+                     sort(array, i - 1, i - 3);
+
+                     }
+                    else if (array[i] == '0' && array[i + 1] == '0') {
                         sort(array, i, i - 3);
-                    }
-                    else if(array[i]=='1' && array[i+1]=='1'){
-                        sort(array, i-1, i - 3);
+
+                    } else if(array[i]=='1' && array[i+1]=='1') {
+                        sort(array, i, i - 2);
                     }
                     else{
-                        // sort(array, i - 2, i);
                         sort(array, i + 1, i - 3);
                     }
-                }
-                else{
-                    if (array[i] == '0' && array[i + 1] == '1') {
+                    /**else{
 
-                        if(i+1 == array.length-1) {
-                            if(array[i+1] == '1') {
+                     // sort(array, i - 2, i);
+                     sort(array, i + 1, i - 3);
+                     }**/
+                }
+
+                    else if (i == array.length - 1) {
+                        if (array[i - 1] == '0' && array[i] == '1') {
+                            sort(array, i - 1, i - 4);
+                            i = -1;
+                        }
+                        /**else if(array[i - 1] == '0' && array[i] == '0'){
+                            sort(array, i - 1, i - 3);
+                        }**/
+                        else /**if(array[i-1]=='1' && array[i]=='1')**/ {
+                            sort(array, i - 2, i - 4);
+                        }
+
+                    }
+         else{
+                /** if (i == array.length - 2) {
+                 if (array[i] == '0' && array[i + 1] == '1' && array[i - 1] == '1') {
+                 sort(array, i, i - 3);
+                 i = 0;
+                 }
+                 /**else if(array[i]=='1' && array[i+1]=='0'){
+                 sort(array, i - 1, i - 3);
+                 }
+                 else if(array[i]=='0' && array[i+1]=='1'){
+                 sort(array, i - 1, i - 3);
+                 }
+                else if (array[i] == '0' && array[i + 1] == '0') {
+                    sort(array, i, i - 3);
+                } else /**(array[i]=='1' && array[i+1]=='1')**/ {
+                    //sort(array, i, i - 2);
+                //}
+                /**else{
+
+                 // sort(array, i - 2, i);
+                 sort(array, i + 1, i - 3);
+                 }
+                 }**/
+
+
+                if (array[i] == '0' && array[i + 1] == '1') {
+
+                    if (i + 1 == array.length - 1) {
+                        if (array[i + 1] == '1') {
+                            sort(array, i - 1, i - 3);
+                        } else if (array[i + 1] == '0') {
+                            sort(array, i, i - 3);
+                        }
+                    } else {
+                        if (array[i + 2] == '0') {
+                            if (i + 1 == array.length - 3) {
+                                sort(array, i, i + 2);
+                            } else if (i + 1 == array.length - 4) {
+                                sort(array, i, array.length - 2);
+                            } else {
+                                sort(array, i, i - 2);
+                            }
+                        }
+                        if (i + 2 == array.length) {
+                            if (array[i - 1] == '0') {
                                 sort(array, i - 1, i - 3);
                             }
-                            else if(array[i+1] == '0'){
-                                sort(array, i , i - 3);
+                        }
+                        if (array[i + 2] == '1') {
+                            if (i + 2 == array.length - 1) {
+                                sort(array, i + 1, i - 2);
+                            } else if (i + 2 == array.length - 3 && array[i + 3] == '1') {
+                                sort(array, i + 1, array.length - 2);
+                            } else if (i + 2 == array.length - 2) {
+                                sort(array, i + 1, i - 1);
+                            } else {
+                                sort(array, i + 1, array.length - 2);
                             }
                         }
-                        else{
-
-                            if(array[i+2]=='0'){
-                                if(i+1 == array.length-3){
-                                    sort(array, i, i+2);
-                                }
-                                else if(i+1 == array.length-4){
-                                    sort(array,i, array.length-2);
-                                }
-                                else{
-                                    sort(array, i, i-2);
-                                }
-                            }
-                            if ( i+2==array.length){
-                                if (array[i-1] == '0' ){
-                                    sort(array, i-1, i-3);
-                                }
-                            }
-                            if (array[i + 2] == '1') {
-                                if(i+2==array.length-1){
-                                    sort(array, i+1, i-2);
-                                }
-                                else if(i+2== array.length-3 && array[i+3] == '1'){
-                                    sort(array, i+1, array.length-2); }
-                                else {
-                                    sort(array, i + 1,  array.length-2); }
-                            }
-                        }
+                    }
                     }
                 }
             }
-        }
+
+            }
+            //System.out.println(Arrays.toString(array));
+            if (isSorted(array, i)) {
+
+                System.out.println("Sorted! " + Arrays.toString(array));
+                System.exit(0);
+            }
     }
 }
+}
+
